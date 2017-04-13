d64311536a944a0822bfe4b72309bf0c438a6fc3
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-04-13 21:27:27 +0300

    upgrade

--- js/cost.js
+++ js/cost.js
@@ -2,13 +2,20 @@ $(document).ready(function() {
 	var cost = $('.cost');
 	var costButton = $('.menu__item--modal');
 	var costClose = $('.cost__close');
+	var costBuy = $('.price__button');
+	var costRegistration = $('.modal-registration');
 
 
 	$(costButton).click(function() {
-		$(cost).removeClass('hide');
+		$(cost).toggleClass('hide');
 	})
 
 	$(costClose).click(function() {
 		$(cost).addClass('hide');
+	})
+
+	$(costBuy).click(function() {
+		$(cost).addClass('hide'),
+		$(costRegistration).removeClass('hide');
 	})
 });
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 	var menuButton = $('.header__menu-button');
 	var menuNav = $('.menu');
-
+	var menuItem = $('.menu__item');
+	var menuPrice = $('.cost');
+	var menuRegistration = $('.modal-registration');
 
 	$(menuButton).click(function() {
-		$(menuNav).toggleClass('menu--hide');
+		$(menuNav).toggleClass('menu--hide'),
+		$(menuPrice).addClass('hide'),
+		$(menuRegistration).addClass('hide');
+	})
+
+	$(menuItem).click(function() {
+		$(menuNav).addClass('menu--hide');
 	})
 });
