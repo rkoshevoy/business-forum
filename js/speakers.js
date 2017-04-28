8e8b8008d2227fdc2a749e31dadd049010b1ebc7
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-04-28 18:17:02 +0300

    updates

--- js/speakers.js
+++ js/speakers.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-	var speaker = $('.speaker');
-	var speakerDetails = $('.speaker-details');
+	var speaker1 = $('#speaker1');
+	var speaker2 = $('#speaker2');
+	var speakerDetails1 = $('#speaker-modal1');
+	var speakerDetails2 = $('#speaker-modal2');
 	var speakerClose = $('.speaker-details__close');
 
-	$(speaker).click(function() {
-		$(speakerDetails).removeClass('hide'),
-		$(speakerDetails).addClass('zoomIn');
+	$(speaker1).click(function() {
+		$(speakerDetails1).removeClass('hide'),
+		$(speakerDetails1).addClass('zoomIn');
+	})
+
+	$(speaker2).click(function() {
+		$(speakerDetails2).removeClass('hide'),
+		$(speakerDetails2).addClass('zoomIn');
 	})
 
 	$(speakerClose).click(function() {
-		$(speakerDetails).addClass('hide');
+		$(speakerDetails1).addClass('hide'),
+		$(speakerDetails2).addClass('hide');
 	})
 });
