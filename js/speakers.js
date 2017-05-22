d0ca1f016a5b2f5d03362b628cf365cd21ba4d0a
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-05-22 13:49:20 +0300

    added speakers, other little fixes

--- js/speakers.js
+++ js/speakers.js
@@ -10,6 +10,30 @@ $(document).ready(function() {
 	var speaker9 = $('#speaker9');
 	var speaker10 = $('#speaker10');
 	var speaker11 = $('#speaker11');
+	var speaker12 = $('#speaker12');
+	var speaker13 = $('#speaker13');
+	var speaker14 = $('#speaker14');
+	var speaker15 = $('#speaker15');
+	var speaker16 = $('#speaker16');
+	var speaker17 = $('#speaker17');
+	var speaker18 = $('#speaker18');
+	var speaker19 = $('#speaker19');
+	var speaker20 = $('#speaker20');
+	var speaker21 = $('#speaker21');
+	var speaker22 = $('#speaker22');
+	var speaker23 = $('#speaker23');
+	var speaker24 = $('#speaker24');
+	var speaker25 = $('#speaker25');
+	var speaker26 = $('#speaker26');
+	var speaker27 = $('#speaker27');
+	var speaker28 = $('#speaker28');
+	var speaker29 = $('#speaker29');
+	var speaker30 = $('#speaker30');
+	var speaker31 = $('#speaker31');
+	var speaker32 = $('#speaker32');
+	var speaker33 = $('#speaker33');
+	var speaker34 = $('#speaker34');
+
 	var speakerDetails1 = $('#speaker-modal1');
 	var speakerDetails2 = $('#speaker-modal2');
 	var speakerDetails3 = $('#speaker-modal3');
@@ -21,6 +45,30 @@ $(document).ready(function() {
 	var speakerDetails9 = $('#speaker-modal9');
 	var speakerDetails10 = $('#speaker-modal10');
 	var speakerDetails11 = $('#speaker-modal11');
+	var speakerDetails12 = $('#speaker-modal12');
+	var speakerDetails13 = $('#speaker-modal13');
+	var speakerDetails14 = $('#speaker-modal14');
+	var speakerDetails15 = $('#speaker-modal15');
+	var speakerDetails16 = $('#speaker-modal16');
+	var speakerDetails17 = $('#speaker-modal17');
+	var speakerDetails18 = $('#speaker-modal18');
+	var speakerDetails19 = $('#speaker-modal19');
+	var speakerDetails20 = $('#speaker-modal20');
+	var speakerDetails21 = $('#speaker-modal21');
+	var speakerDetails22 = $('#speaker-modal22');
+	var speakerDetails23 = $('#speaker-modal23');
+	var speakerDetails24 = $('#speaker-modal24');
+	var speakerDetails25 = $('#speaker-modal25');
+	var speakerDetails26 = $('#speaker-modal26');
+	var speakerDetails27 = $('#speaker-modal27');
+	var speakerDetails28 = $('#speaker-modal28');
+	var speakerDetails29 = $('#speaker-modal29');
+	var speakerDetails30 = $('#speaker-modal30');
+	var speakerDetails31 = $('#speaker-modal31');
+	var speakerDetails32 = $('#speaker-modal32');
+	var speakerDetails33 = $('#speaker-modal33');
+	var speakerDetails34 = $('#speaker-modal34');
+
 	var speakerClose = $('.speaker-details__close');
 
 	$(speaker1).click(function() {
@@ -78,6 +126,121 @@ $(document).ready(function() {
 		$(speakerDetails11).addClass('zoomIn');
 	})
 
+	$(speaker12).click(function() {
+		$(speakerDetails12).removeClass('hide');
+		$(speakerDetails12).addClass('zoomIn');
+	})
+
+	$(speaker13).click(function() {
+		$(speakerDetails13).removeClass('hide');
+		$(speakerDetails13).addClass('zoomIn');
+	})
+
+	$(speaker14).click(function() {
+		$(speakerDetails14).removeClass('hide');
+		$(speakerDetails14).addClass('zoomIn');
+	})
+
+	$(speaker15).click(function() {
+		$(speakerDetails15).removeClass('hide');
+		$(speakerDetails15).addClass('zoomIn');
+	})
+
+	$(speaker16).click(function() {
+		$(speakerDetails16).removeClass('hide');
+		$(speakerDetails16).addClass('zoomIn');
+	})
+
+	$(speaker17).click(function() {
+		$(speakerDetails17).removeClass('hide');
+		$(speakerDetails17).addClass('zoomIn');
+	})
+
+	$(speaker18).click(function() {
+		$(speakerDetails18).removeClass('hide');
+		$(speakerDetails18).addClass('zoomIn');
+	})
+
+	$(speaker19).click(function() {
+		$(speakerDetails19).removeClass('hide');
+		$(speakerDetails19).addClass('zoomIn');
+	})
+
+	$(speaker20).click(function() {
+		$(speakerDetails20).removeClass('hide');
+		$(speakerDetails20).addClass('zoomIn');
+	})
+
+	$(speaker21).click(function() {
+		$(speakerDetails21).removeClass('hide');
+		$(speakerDetails21).addClass('zoomIn');
+	})
+
+	$(speaker22).click(function() {
+		$(speakerDetails22).removeClass('hide');
+		$(speakerDetails22).addClass('zoomIn');
+	})
+
+	$(speaker23).click(function() {
+		$(speakerDetails23).removeClass('hide');
+		$(speakerDetails23).addClass('zoomIn');
+	})
+
+	$(speaker24).click(function() {
+		$(speakerDetails24).removeClass('hide');
+		$(speakerDetails24).addClass('zoomIn');
+	})
+
+	$(speaker25).click(function() {
+		$(speakerDetails25).removeClass('hide');
+		$(speakerDetails25).addClass('zoomIn');
+	})
+
+	$(speaker26).click(function() {
+		$(speakerDetails26).removeClass('hide');
+		$(speakerDetails26).addClass('zoomIn');
+	})
+
+	$(speaker27).click(function() {
+		$(speakerDetails27).removeClass('hide');
+		$(speakerDetails27).addClass('zoomIn');
+	})
+
+	$(speaker28).click(function() {
+		$(speakerDetails28).removeClass('hide');
+		$(speakerDetails28).addClass('zoomIn');
+	})
+
+	$(speaker29).click(function() {
+		$(speakerDetails29).removeClass('hide');
+		$(speakerDetails29).addClass('zoomIn');
+	})
+
+	$(speaker30).click(function() {
+		$(speakerDetails30).removeClass('hide');
+		$(speakerDetails30).addClass('zoomIn');
+	})
+
+	$(speaker31).click(function() {
+		$(speakerDetails31).removeClass('hide');
+		$(speakerDetails31).addClass('zoomIn');
+	})
+
+	$(speaker32).click(function() {
+		$(speakerDetails32).removeClass('hide');
+		$(speakerDetails32).addClass('zoomIn');
+	})
+
+	$(speaker33).click(function() {
+		$(speakerDetails33).removeClass('hide');
+		$(speakerDetails33).addClass('zoomIn');
+	})
+
+	$(speaker34).click(function() {
+		$(speakerDetails34).removeClass('hide');
+		$(speakerDetails34).addClass('zoomIn');
+	})
+
 	$(speakerClose).click(function() {
 		$(speakerDetails1).addClass('hide');
 		$(speakerDetails2).addClass('hide');
@@ -90,5 +253,28 @@ $(document).ready(function() {
 		$(speakerDetails9).addClass('hide');
 		$(speakerDetails10).addClass('hide');
 		$(speakerDetails11).addClass('hide');
+		$(speakerDetails12).addClass('hide');
+		$(speakerDetails13).addClass('hide');
+		$(speakerDetails14).addClass('hide');
+		$(speakerDetails15).addClass('hide');
+		$(speakerDetails16).addClass('hide');
+		$(speakerDetails17).addClass('hide');
+		$(speakerDetails18).addClass('hide');
+		$(speakerDetails19).addClass('hide');
+		$(speakerDetails20).addClass('hide');
+		$(speakerDetails21).addClass('hide');
+		$(speakerDetails22).addClass('hide');
+		$(speakerDetails23).addClass('hide');
+		$(speakerDetails24).addClass('hide');
+		$(speakerDetails25).addClass('hide');
+		$(speakerDetails26).addClass('hide');
+		$(speakerDetails27).addClass('hide');
+		$(speakerDetails28).addClass('hide');
+		$(speakerDetails29).addClass('hide');
+		$(speakerDetails30).addClass('hide');
+		$(speakerDetails31).addClass('hide');
+		$(speakerDetails32).addClass('hide');
+		$(speakerDetails33).addClass('hide');
+		$(speakerDetails34).addClass('hide');
 	})
 });
