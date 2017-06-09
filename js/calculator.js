481d13616175225fe8a5544ce49afc17ddd49fa1
111 <111>
2017-06-09 13:56:37 +0300

    билеты с кофе брейками

--- js/calculator.js
+++ js/calculator.js
@@ -1,46 +1,40 @@
-$(document).ready(function() {  
-    var calculator = function() {
-        var tickets = $("input[name='tickets']").val();
-        var calc = $(".modal-registration__result");
+$(document).ready(function() {
+	function calculator() {
+        var tickets = $("input[name='tickets']").val(),
+        	calc = $(".modal-registration__result"),
+	    	itemIndex,
+        	result;
 
-	    var dateNow = new Date();
-	    var currentMonth = dateNow.getMonth() + 1;
+       	var dateNow = new Date();
 	    var currentDay = dateNow.getDate();
-	    var itemIndex;
-	    
-        var result;
 
-        switch(currentMonth){
-	        case 4:
-		        itemIndex = 1;
-		        break;
-	        case 5:
-		        itemIndex = 2;
-		        break;
-	    	case 6:
-		    	if (currentDay < 20) {
-		    		itemIndex = 2;
-		    	}
-		    	if (currentDay >= 20 ) {
-		    		itemIndex = 3;
-		    	}
-		        break;
-		    default:
-		    	itemIndex = 0;
-		    	break;
-        }
+        if (currentDay < 20) itemIndex = 0;
+        if (currentDay >= 20 ) itemIndex = 1;
 
         var one_day_price = parseInt($('#one_day_price li:eq(' + itemIndex + ') p').text());
         var two_days_price = parseInt($('#two_days_price li:eq(' + itemIndex + ') p').text());
+        var one_day_price_with_coffee = parseInt($('#one_day_price_second li:eq(' + itemIndex + ') p').text());
+        var two_days_price_with_coffee = parseInt($('#two_days_price_second li:eq(' + itemIndex + ') p').text());
 
         if($('#one_ticket').is(":checked")){
             result = tickets * one_day_price;
-        } else if ($('#two_tickets').is(":checked")){
+        } 
+        if ($('#two_tickets').is(":checked")){
             result = tickets * two_days_price;
         }
+        if ($('#one_ticket_second').is(":checked")){
+            result = tickets * one_day_price_with_coffee;
+        }
+        if ($('#two_tickets_second').is(":checked")){
+            result = tickets * two_days_price_with_coffee;
+        }
+
+        if (tickets > 2) {
+        	result *= 0.85;
+        }
+
         $(calc).text(result + " грн").val();
         $("input[name='summ']").val(result);
-
     };
 
     $("input[name='tickets']")
@@ -48,4 +42,7 @@ $(document).ready(function() {
         .click(calculator);
     $('#one_ticket').click(calculator);
     $('#two_tickets').click(calculator);
+    $('#one_ticket_second').click(calculator);
+    $('#two_ticket_second').click(calculator);
+   
 });
